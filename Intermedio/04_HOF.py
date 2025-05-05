ed249ecb4176208c8754346fad69584141837d35
Andres <andrex.ao@gmail.com>
2025-05-04 21:37:14 -0500

    solved all other exercises for higher order functions

--- Intermedio/04_HOF.py
+++ Intermedio/04_HOF.py
@@ -47,13 +47,54 @@ print(sum_of_numbers)
 
 # Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and Iceland are north European countries
 def countries_as_text(lst):
-    initial_list = reduce(lambda a, b: a + ", " + b, lst[:-1])
+    long_list = lst[:-1]
     last_element = lst[-1]
+    initial_list = reduce(lambda a, b: a + ", " + b, long_list)
     text = f"{initial_list} and {last_element} are north European countries"
     return text
 print(countries_as_text(countries))
 
 # Declare a function called categorize_countries that returns a list of countries with some common pattern (you can find the countries list in this repository as countries.js(eg 'land', 'ia', 'island', 'stan')).
+import countries
+def categorize_countries(countries_list):
+    return filter(lambda country: country.startswith("K"), countries_list)
+
+print(list(categorize_countries(countries.countries_list)))
+
 # Create a function returning a dictionary, where keys stand for starting letters of countries and values are the number of country names starting with that letter.
+import string
+
+def countries_by_letter(countries_list):
+    countries_dct = {}
+    for letter in string.ascii_uppercase:
+        count_by_letter = len(list(filter(lambda country: country.startswith(letter), countries_list)))
+        countries_dct[letter] = count_by_letter
+    return countries_dct
+print(countries_by_letter(countries.countries_list))
+
+# V2 GPT
+from collections import Counter
+
+def countries_by_letter_2(countries_list):
+    first_letters = [country[0].upper() for country in countries_list]
+    return dict(Counter(first_letters))
+print(countries_by_letter_2(countries.countries_list))
+
 # Declare a get_first_ten_countries function - it returns a list of first ten countries from the countries.js list in the data folder.
+def get_first_ten_countries(countries_list):
+    return countries_list[0:10]
+print(get_first_ten_countries(countries.countries_list))
+
+# V2
+'''
+it can be expressed using a higher-order function like itertools.islice, which takes an iterable and returns the first n elements without slicing the list directly. This can be useful if you're working with large iterables or want a more functional approach.
+'''
+from itertools import islice
+def get_first_ten_countries_2(countries_list):
+    return islice(countries_list, 10)
+print(list(get_first_ten_countries_2(countries.countries_list)))
+
 # Declare a get_last_ten_countries function that returns the last ten countries in the countries list.
+def get_last_ten_countries(countries_list):
+    return countries_list[-10:]
+print(get_last_ten_countries(countries.countries_list))
